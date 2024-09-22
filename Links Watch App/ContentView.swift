db673558d903d115adb15b8ab8b2a184311ac82a
Rhys <98863820+rhysdh540@users.noreply.github.com>
2024-09-22 15:33:31 -0400

    Initial Commit

---
+++ Links Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Links Watch App
+//
+//  Created by Rhys de Haan on 9/22/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Links Watch App/LinksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinksApp.swift
+//  Links Watch App
+//
+//  Created by Rhys de Haan on 9/22/24.
+//
+
+import SwiftUI
+
+@main
+struct Links_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
